0f0bd0d6467cc9987440cb49b1469b5cc2b6224e
GeorgeTheSlayer <holland@sersen.net>
2023-02-22 21:14:40 -0600

    trying again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,10 +18,6 @@
     }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
 <button on:click={onClick}>Start</button>
 <p>{x}</p>
 <p>{y}</p>
